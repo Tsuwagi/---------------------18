e372b0d11c6062592d0d1deb865d3a525af90df9
Tsuwagi <viktoria16kutas5@gmail.com>
2025-03-25 01:52:38 +0200

    real fish 3

--- 18(3).cpp
+++ 18(3).cpp
@@ -3,60 +3,64 @@
 using namespace std;
 
 void ggwpswapper(int a[5][4]) {
-    int min = 9999, max = 0, length = 0, index1, index2;
-    for(int i = 0; i < 5; i++) {
-        for(int n = 0; n < 4; n++) {
-           int temp = a[i][n];
-           while(temp % 10 != 0) { // удаляет последнюю цифру 
-                temp /= 10;
-                length++; // записывает сколько раз удалили последнюю цифру
-           }
+    int min = 9999, max = 0;
+    int index1 = 0, index2 = 0;
+
+    for (int i = 0; i < 5; i++) {
+        int totalLength = 0;
+        for (int j = 0; j < 4; j++) {
+            int number = a[i][j];
+            int length = 0;
+
+            while (number != 0) { // Подсчет количества цифр
+                number /= 10;
+                length++;
+            }
+
+            totalLength += length; //сума всех чисел строки
         }
-      if(max < length) {
-            max = length;
+
+        if (totalLength > max) { 
+            max = totalLength;
             index1 = i;
-       } 
-       if(min > length) {
-            min = length;
+        }
+        if (totalLength < min) {
+            min = totalLength;
             index2 = i;
-       }
-       length = 0;
+        }
     }
 
-    for(int i = 0; i < 4; i++) {
-        int temp = a[index1][i];
-        a[index1][i] = a[index2][i];
-        a[index2][i] = a[index1][i];
-    }  
-
+    for (int i = 0; i < 4; i++) {
+        a[index2][i] = a[index1][i]; //дублироование наибольшей строки в наименьшую
+    }
 }
 
-int main()
-{   
+int main() {   
     int a[5][4] = {
-        {1,1,10,1},
-        {1,10,10,1},
-        {1,10,10,10},
-        {1,1,10,1},
-        {1,1,1,1}
+        {1, 1, 12, 1},
+        {1, 12, 12, 1},
+        {1, 12, 12, 12},
+        {1, 1, 12, 1},
+        {1, 1, 1, 1}
     };
 
-    cout << "Matrix:\n";
+    cout << "Original Matrix:\n";
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 4; j++) {
-            cout << a[i][j];
+            cout << a[i][j] << " ";
         }
         cout << endl;
     }
 
     ggwpswapper(a);
 
-    cout << "new Matrix:\n";
-    for(int i = 0; i < 5; i++) {
-        for(int n = 0; n < 4; n++) {
-            cout << a[i][n];
+    cout << "New Matrix:\n";
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 4; j++) {
+            cout << a[i][j] << " ";
         }
         cout << endl;
     } 
+
     return 0;
 }
